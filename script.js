353707ec17138f43f818e558d7bfb680e36ed6df
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-09 21:46:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+document.getElementById('addTodoBtn').addEventListener('click',function(){
+	var todoInput = document.getElementById('newTodoInput');
+	var todoText = todoInput.value.trim();
 
+	if(todoText !== ''){
+		var todoList = document.getElementById('todoList');
+		var newTodoItem = document.createElement('li');
+		newTodoItem.textContent = todoText;
+		todoList.appendChild(newTodoItem);
+		todoInput.value = '';
+	}else{
+		alert('Please enter s valid to-do item.');
+	}
+});
